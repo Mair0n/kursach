450a0194f6299bd1443ed0b6cd27703d235c7bbb
misha.mironenko <misha.mironenko@gmail.com>
2019-06-04 04:27:30 +0300

    Initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { AnimalsComponent } from './animals/animals.component';
+import { AnimalsCreatorComponent } from './animals-creator/animals-creator.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/animals', pathMatch: 'full' },
+  { path:'animals', component: AnimalsComponent },
+  { path:'animals/new', component: AnimalsCreatorComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
